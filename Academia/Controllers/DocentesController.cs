305577c466d3a0144a0a3ee8fc7c01634ecad5a5
Engler Gonzalez <englergonzalez@hotmail.com>
2020-10-31 02:34:17 -0500

    Agregar archivos de proyecto.

---
+++ Academia/Controllers/DocentesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Academia.Models.db;
+
+namespace Academia.Controllers
+{
+    public class DocentesController : Controller
+    {
+        private AcademiaEntities db = new AcademiaEntities();
+
+        // GET: Docentes
+        public ActionResult Index()
+        {
+            var docente = db.Docente.Include(d => d.Dedicacion).Include(d => d.TipoSangre);
+            return View(docente.ToList());
+        }
+
+        // GET: Docentes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Docente docente = db.Docente.Find(id);
+            if (docente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(docente);
+        }
+
+        // GET: Docentes/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_dedicacion = new SelectList(db.Dedicacion, "id", "descripcion");
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre");
+            return View();
+        }
+
+        // POST: Docentes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,Salario,eshombre,id_dedicacion,especialidad,fechanacimiento,id_tipoSangre")] Docente docente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Docente.Add(docente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_dedicacion = new SelectList(db.Dedicacion, "id", "descripcion", docente.id_dedicacion);
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre", docente.id_tipoSangre);
+            return View(docente);
+        }
+
+        // GET: Docentes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Docente docente = db.Docente.Find(id);
+            if (docente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_dedicacion = new SelectList(db.Dedicacion, "id", "descripcion", docente.id_dedicacion);
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre", docente.id_tipoSangre);
+            return View(docente);
+        }
+
+        // POST: Docentes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,Salario,eshombre,id_dedicacion,especialidad,fechanacimiento,id_tipoSangre")] Docente docente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(docente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_dedicacion = new SelectList(db.Dedicacion, "id", "descripcion", docente.id_dedicacion);
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre", docente.id_tipoSangre);
+            return View(docente);
+        }
+
+        // GET: Docentes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Docente docente = db.Docente.Find(id);
+            if (docente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(docente);
+        }
+
+        // POST: Docentes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Docente docente = db.Docente.Find(id);
+            db.Docente.Remove(docente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Academia/Controllers/EstudiantesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Academia.Models.db;
+
+namespace Academia.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private AcademiaEntities db = new AcademiaEntities();
+
+        // GET: Estudiantes
+        public ActionResult Index()
+        {
+            var estudiante = db.Estudiante.Include(e => e.TipoSangre);
+            return View(estudiante.ToList());
+        }
+
+        // GET: Estudiantes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiante.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // GET: Estudiantes/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre");
+            return View();
+        }
+
+        // POST: Estudiantes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,fechanacimiento,promedionotas,eshombre,id_tipoSangre")] Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Estudiante.Add(estudiante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre", estudiante.id_tipoSangre);
+            return View(estudiante);
+        }
+
+        // GET: Estudiantes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiante.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre", estudiante.id_tipoSangre);
+            return View(estudiante);
+        }
+
+        // POST: Estudiantes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,fechanacimiento,promedionotas,eshombre,id_tipoSangre")] Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudiante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_tipoSangre = new SelectList(db.TipoSangre, "id", "nombre", estudiante.id_tipoSangre);
+            return View(estudiante);
+        }
+
+        // GET: Estudiantes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiante.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // POST: Estudiantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Estudiante estudiante = db.Estudiante.Find(id);
+            db.Estudiante.Remove(estudiante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
